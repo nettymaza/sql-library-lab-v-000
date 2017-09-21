b8bb1a7446684a200702ebeb5dcfa1a9bb678270
Annette Maza <nettymaza@gmail.com>
2017-09-21 17:08:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Game of Thrones", 3, "Scottish Fold", 1000000);
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (2, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Game of Thrones", 1, 1);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (2, "Harry Potter", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
-INSERT INTO subgenres (id, name) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO subgenres (id, name) VALUES (1, "Fantasy");
+INSERT INTO subgenres (id, name) VALUES (2, "Mystery");
 
 INSERT INTO authors (id, name) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
 INSERT INTO authors (id, name) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
