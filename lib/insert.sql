6ac4323158c184daba8873abfb039115edc44c94
Annette Maza <nettymaza@gmail.com>
2017-09-21 17:37:51 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -14,5 +14,5 @@ INSERT INTO books (id, title, year, series_id) VALUES (4, "The Philosoper's Ston
 INSERT INTO books (id, title, year, series_id) VALUES (5, "The Chamber of Secrets", 1998, 2);
 INSERT INTO books (id, title, year, series_id) VALUES (6, "The Prisoner of Azkaban", 1999, 2);
 
-INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES (1, "Jon Snow", "Dragon", "Winter is Coming" 1, 1), (2, "Arya Stark", "human", "Light like the wind" 1, 1), (3, "Daenerys Targaryen", "Dagron", "I am mother of Dragons" 1, 1), (4, "Tyrion Lannister", "Imp", "A Lannister always pays his debts" 1, 1);
+INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES (1, "Jon Snow", "Dragon", "Winter is Coming" 1, 1), (2, "Arya Stark", "human", "Light like the wind" 1, 1), (3, "Daenerys Targaryen", "Dragon", "I am mother of Dragons" 1, 1), (4, "Tyrion Lannister", "Imp", "A Lannister always pays his debts" 1, 1);
 INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES (5, "Harry Potter", "Wizard", "motto1", 2, 2), (6, "Ron Weasley", "human", "motto2", 2, 2), (7, "Hermione Granger", "human", "motto3", 2, 2), (8, "Lord Voldemort", "Sorcerer", "motto4", 2, 2);
