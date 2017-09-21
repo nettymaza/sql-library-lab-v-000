ce2d7eaec4884c469aa9fc0c6d98d312d6cebb1f
Annette Maza <nettymaza@gmail.com>
2017-09-21 17:21:07 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,9 +7,9 @@ INSERT INTO subgenres (id, name) VALUES (2, "Mystery");
 INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin");
 INSERT INTO authors (id, name) VALUES (2, "J. K. Rowling");
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "A Game of Thrones", 3, "Scottish Fold", 1000000);
-INSERT INTO books (id, title, year, series_id) VALUES (2, "A Clash of Kings", 3, "Scottish Fold", 1000000);
-INSERT INTO books (id, title, year, series_id) VALUES (3, "A Storm of Swords", 3, "Scottish Fold", 1000000);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "A Game of Thrones", 1996, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (2, "A Clash of Kings", 1998, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (3, "A Storm of Swords", 2000, 1);
 INSERT INTO books (id, title, year, series_id) VALUES (4, "The Philosoper's Stone", 1997, 2);
 INSERT INTO books (id, title, year, series_id) VALUES (5, "The Chamber of Secrets", 1998, 2);
 INSERT INTO books (id, title, year, series_id) VALUES (6, "The Prisoner of Azkaban", 1999, 2);
