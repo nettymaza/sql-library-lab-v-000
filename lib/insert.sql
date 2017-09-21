208fef7ad69b3cc1106feb47343f0b59dc3e599c
Annette Maza <nettymaza@gmail.com>
2017-09-21 17:14:44 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,9 +7,9 @@ INSERT INTO subgenres (id, name) VALUES (2, "Mystery");
 INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin");
 INSERT INTO authors (id, name) VALUES (2, "J. K. Rowling");
 
--- INSERT INTO books (id, title, year, series_id) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
--- INSERT INTO books (id, title, year, series_id) VALUES (2, "Maru", 3, "Scottish Fold", 1000000);
--- INSERT INTO books (id, title, year, series_id) VALUES (3, "Maru", 3, "Scottish Fold", 1000000);
--- INSERT INTO books (id, title, year, series_id) VALUES (4, "Maru", 3, "Scottish Fold", 1000000);
--- INSERT INTO books (id, title, year, series_id) VALUES (5, "Maru", 3, "Scottish Fold", 1000000);
--- INSERT INTO books (id, title, year, series_id) VALUES (6, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO books (id, title, year, series_id) VALUES (2, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO books (id, title, year, series_id) VALUES (3, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO books (id, title, year, series_id) VALUES (4, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO books (id, title, year, series_id) VALUES (5, "Maru", 3, "Scottish Fold", 1000000);
+INSERT INTO books (id, title, year, series_id) VALUES (6, "Maru", 3, "Scottish Fold", 1000000);
